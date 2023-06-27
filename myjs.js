e4ec6ce707bfa4b9db685a50ed95c22f00ca3c85
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-06-27 09:26:35 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,13 +1,18 @@
 alert('Hello, Chinatsu!');
 alert(1+1);
 alert(Math.sqrt(1+1));
+
 alert(2+3);
 alert(Math.sqrt(2+3));
+
 alert('2'+'3');
 alert(Math.sqrt('2'+'3'));
+
 alert(Number('2')+Number('3'));
-// alert(Math.sqrt(Number('2')+Number('3')));
+// alert(Math.sqrt(Number('2')+Number('3'))
+
 alert('2'+3);
 alert(Math.sqrt('2'+3);
+
 alert(Number('2')+3);
 alert(Math.sqrt(Number('2')+3));

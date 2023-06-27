bf119777c83be7d3b9df6055f920829a68a5ee6d
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-06-27 09:39:16 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -13,7 +13,7 @@ alert(Number('2')+Number('3'));
 // alert(Math.sqrt(Number('2')+Number('3'))
 
 alert('2'+3);
-alert(Math.sqrt('2'+3);
+alert(Math.sqrt('2'+3));
 
 alert(Number('2')+3);
 alert(Math.sqrt(Number('2')+3));
